8f464c185e142e4b6e0afba6aca79c0b93c6a08b
patrick hrabos <phrabos@gmail.com>
2021-05-19 14:20:51 -0400

    add nav buttons

--- src/containers/AnimalCrossingContainer.jsx
+++ src/containers/AnimalCrossingContainer.jsx
@@ -7,17 +7,25 @@ import style from './main.css';
 const AnimalCrossingContainer = () => {
   const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetchAllCharacters(1)
+    fetchAllCharacters(page)
       .then(setCharacters)
       .finally(() => setLoading(false));
-  }, []);
+  }, [page]);
+
+  console.log(page);
   
   if(loading) return <h1>Loading...</h1>;
   
   return (
     <div className={style.characterWrapper}>
+      <button onClick={() => {
+        if(page === 1) return;
+        else setPage((prevPage) => prevPage - 1);
+      }}>Prev</button>
+      <button onClick={() => setPage((prevPage) => prevPage + 1)}>Next</button>
       <CharacterList characters={characters}/>
     </div>
   );
